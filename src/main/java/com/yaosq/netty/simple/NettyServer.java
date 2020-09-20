5f9f7d7a5c5014db5c4266cb3510e0b507c0ac9a
yaoshuangqi <1575848467@qq.com>
2020-09-20 20:39:30 +0800

    netty的异步任务;

--- src/main/java/com/yaosq/netty/simple/NettyServer.java
+++ src/main/java/com/yaosq/netty/simple/NettyServer.java
@@ -2,6 +2,7 @@ package com.yaosq.netty.simple;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -26,20 +27,32 @@ public class NettyServer {
 
             serverBootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .option(ChannelOption.SO_BACKLOG, 128)//初始化服务器可连接的队列大小，因为服务器处理客户端是顺序处理的，同一时间只能处理一个客户端
                     .childOption(ChannelOption.SO_KEEPALIVE, true)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                            System.out.println("==> 每一个客户连接到socketChannel hashCode都是不同的，hashCode="+socketChannel.hashCode());
+                            //这里可以将每一个channel管理起来，用作之后指定客户端的业务异步推送
                             socketChannel.pipeline().addLast(new NettyServerHandler());
                         }
                     });
             System.out.println("==> netty server is ready...");
             //绑定端口+同步，启动netty服务
+            //ChannelFuture 一个异步结果
             ChannelFuture channelFuture = serverBootstrap.bind(6666).sync();
 
+            //异步模型
+            channelFuture.addListener((channelFuture0) -> {
+                if(channelFuture0.isSuccess()){
+                    System.out.println("==> 端口绑定成功");
+                }
+            });
+
             //关闭通道并进行监听
-            channelFuture.channel().closeFuture().sync();
+            //channelFuture.channel().closeFuture().sync();
+
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
--- src/main/java/com/yaosq/netty/simple/NettyServerHandler.java
+++ src/main/java/com/yaosq/netty/simple/NettyServerHandler.java
@@ -3,12 +3,8 @@ package com.yaosq.netty.simple;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
-
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 
 /**
  * @author quanroong.ysq
@@ -19,6 +15,7 @@ import java.nio.charset.Charset;
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     /**
     * @description 从客户端发送过来的通道读取消息
+     * channelHandlerContext：包含一个具体的ChannelHandler,
     * @author quanroong.ysq
     * @createtime 2020/8/23 22:42
     * @version 1.0.0
@@ -27,6 +24,28 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
         ByteBuf buf = (ByteBuf)o;
         System.out.println("客户端发送的消息：" + buf.toString(CharsetUtil.UTF_8));
+        //比如：有一个非常耗时的业务，需要进行异步执行 ==> 提交channel 对应NioEventLoop 中的taskQueue中
+
+        //解决方案一：用户自定义一个普通任务
+        channelHandlerContext.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //模拟非常耗时的任务
+                    Thread.sleep(10*1000);
+                    channelHandlerContext.writeAndFlush(Unpooled.copiedBuffer("hello, 我是异步执行任务的", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //可以加多个这样的异步任务，但是要知道，所有的任务都被加入到了NioEventLoop==>channel==>eventLoop==>taskQueue中了，并且是同一个线程
+        //如果下一个任务sleep 20*1000，那么这个任务需要等待30s后执行
+
+        //解决方案二：定时任务
+
+        System.out.println("==> go on execute other task.....");
+
     }
     /**
     * @description 消息读取完成后，做一个消息回发
@@ -36,7 +55,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     */
     @Override
     public void channelReadComplete(ChannelHandlerContext channelHandlerContext) throws Exception {
-        channelHandlerContext.writeAndFlush(Unpooled.copiedBuffer("hello , 客户端。你好", CharsetUtil.UTF_8));
+        channelHandlerContext.writeAndFlush(Unpooled.copiedBuffer("hello , 本次channel读取完成", CharsetUtil.UTF_8));
     }
 
     @Override
